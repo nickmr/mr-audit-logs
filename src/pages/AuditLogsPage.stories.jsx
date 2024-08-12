a3a08ec24a220f160729092657b63044ed147e2a
Nick Dollarhide <nick@metarouter.io>
2024-08-12 10:14:56 -0500

    feat: initial commit

---
+++ src/pages/AuditLogsPage.stories.jsx
@@ -0,0 +1,12 @@
+import AuditLogsPage from "./AuditLogsPage";
+
+export default {
+  title: 'Pages/AuditLogsPage',
+  component: AuditLogsPage,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {};
